fe9d3d180acfb56e0b0164e5b321073f50320a48
Shrugal <kontakt@nhonnef.de>
2023-12-31 00:26:51 +0100

    Update Plugin.cs

--- src/Lidarr.Plugin.Deemix/Plugin.cs
+++ src/Lidarr.Plugin.Deemix/Plugin.cs
@@ -5,7 +5,7 @@ namespace NzbDrone.Core.Plugins
     public class DeemixPlugin : Plugin
     {
         public override string Name => "Deemix";
-        public override string Owner => "ta264";
-        public override string GithubUrl => "https://github.com/ta264/Lidarr.Plugin.Deemix";
+        public override string Owner => "shrugal";
+        public override string GithubUrl => "https://github.com/shrugal/Lidarr.Plugin.Deemix";
     }
 }
